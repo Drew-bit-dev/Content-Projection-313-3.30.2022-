789af8770b2178ecd325032135efba91a39ef721
Drew-bit-dev <d.s.grondahl01@gmail.com>
2022-03-30 13:50:58 -0500

    3 files modified

---
+++ src/app/greeting/greeting.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+import { VERSION } from '@angular/core';
+@Component({
+  selector: 'app-Greeting',
+  template: `
+            <h1> This is a Header in Greeting Component </h1>
+  `,
+})
+export class GreetingComponent {
+  constructor() {}
+}
